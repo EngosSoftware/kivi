931fb7272d7180f4a19d0521c88c41547827ced0
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2024-11-22 18:42:23 +0100

    Updates.

--- src/loader.rs
+++ src/loader.rs
@@ -11,100 +11,102 @@ enum State {
   ValueExt,
 }
 
+macro_rules! consume_char {
+  ($buffer:ident, $ch:expr) => {{
+    $buffer.push($ch);
+  }};
+}
+
+macro_rules! consume_key {
+  ($key:ident, $buffer:ident, $state:ident) => {{
+    $key.clear();
+    $key.push_str(&$buffer);
+    $buffer.clear();
+    $state = State::Value;
+  }};
+}
+
+macro_rules! consume_non_empty_key {
+  ($key:ident, $buffer:ident, $state:ident) => {{
+    $buffer = $buffer.trim().to_string();
+    if !$buffer.is_empty() {
+      consume_key!($key, $buffer, $state);
+    }
+  }};
+}
+
+macro_rules! consume_value {
+  ($output:ident, $key:ident, $buffer:ident, $state:ident) => {{
+    $output.kv.insert($key.clone(), $buffer.clone());
+    $key.clear();
+    $buffer.clear();
+    $state = State::Key;
+  }};
+}
+
+macro_rules! consume_non_empty_value {
+  ($output:ident, $key:ident, $buffer:ident, $state:ident) => {{
+    $buffer = $buffer.trim().to_string();
+    if !$buffer.is_empty() {
+      consume_value!($output, $key, $buffer, $state);
+    }
+  }};
+}
+
+macro_rules! consume_escaped_quotation_mark {
+  ($chars:ident, $buffer:ident) => {{
+    if let Some(ch) = $chars.peek() {
+      if *ch == '"' {
+        $buffer.push('"');
+        $chars.next();
+      } else {
+        $buffer.push('\\');
+      }
+    } else {
+      $buffer.push('\\');
+    }
+  }};
+}
+
+macro_rules! clear_buffer {
+  ($buffer:ident, $state:ident, $next_state:expr) => {{
+    $buffer.clear();
+    $state = $next_state;
+  }};
+}
+
 pub fn load_from_string(input: &str) -> KeyValuePairs {
   let mut output = KeyValuePairs::new();
   let mut state = State::Key;
   let mut buffer = String::new();
   let mut key = String::new();
   let mut chars = input.chars().peekable();
-
   while let Some(ch) = chars.next() {
     match state {
       State::Key => match ch {
-        '"' => {
-          buffer.clear();
-          state = State::KeyExt;
-        }
-        '\n' => {
-          buffer = buffer.trim().to_string();
-          if !buffer.is_empty() {
-            key.clear();
-            key.push_str(&buffer);
-            buffer.clear();
-            state = State::Value;
-          }
-        }
-        other => buffer.push(other),
+        '"' => clear_buffer!(buffer, state, State::KeyExt),
+        '\n' => consume_non_empty_key!(key, buffer, state),
+        other => consume_char!(buffer, other),
       },
       State::KeyExt => match ch {
-        '"' => {
-          key.clear();
-          key.push_str(&buffer);
-          buffer.clear();
-          state = State::Value;
-        }
-        '\\' => {
-          if let Some(nch) = chars.peek() {
-            if *nch == '"' {
-              buffer.push('"');
-              chars.next();
-              continue; // WATCH OUT!
-            }
-          }
-          buffer.push('\\');
-        }
-        other => buffer.push(other),
+        '"' => consume_key!(key, buffer, state),
+        '\\' => consume_escaped_quotation_mark!(chars, buffer),
+        other => consume_char!(buffer, other),
       },
       State::Value => match ch {
-        '"' => {
-          buffer.clear();
-          state = State::ValueExt;
-        }
-        '\n' => {
-          buffer = buffer.trim().to_string();
-          if !buffer.is_empty() {
-            // Consume the key and value.
-            output.kv.insert(key.clone(), buffer.clone());
-            key.clear();
-            buffer.clear();
-            state = State::Key;
-          }
-          buffer.clear();
-        }
+        '"' => clear_buffer!(buffer, state, State::ValueExt),
+        '\n' => consume_non_empty_value!(output, key, buffer, state),
         other => {
-          buffer.push(other);
+          consume_char!(buffer, other);
           if chars.peek().is_none() {
-            // If there are no more characters on input,
-            // then consume the key and value.
-            buffer = buffer.trim().to_string();
-            if !buffer.is_empty() {
-              output.kv.insert(key.clone(), buffer.clone());
-              key.clear();
-              buffer.clear();
-              state = State::Key;
-            }
-            buffer.clear();
+            consume_non_empty_value!(output, key, buffer, state);
           }
         }
       },
       State::ValueExt => match ch {
-        '"' => {
-          output.kv.insert(key.clone(), buffer.clone());
-          key.clear();
-          buffer.clear();
-          state = State::Key;
-        }
-        '\\' => {
-          if let Some(nch) = chars.peek() {
-            if *nch == '"' {
-              buffer.push('"');
-              chars.next();
-              continue; // WATCH OUT!
-            }
-          }
-          buffer.push('\\');
-        }
-        other => buffer.push(other),
+        '"' => consume_value!(output, key, buffer, state),
+        '\\' => consume_escaped_quotation_mark!(chars, buffer),
+        other => consume_char!(buffer, other),
       },
     }
   }
