323062d9387c9061623fce81582a06205dcaf2e1
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-05-19 13:18:07 +0200

    Updates.

--- src/loader.rs
+++ src/loader.rs
@@ -1,79 +1,10 @@
 //! # Implementation of KIVI deserialization functions
 
 use crate::model::KeyValuePairs;
+use std::iter::Peekable;
 use std::path::Path;
+use std::str::Chars;
 use std::{fs, io};
-
-enum State {
-  Key,
-  KeyExt,
-  Value,
-  ValueExt,
-}
-
-macro_rules! consume_char {
-  ($buffer:ident, $ch:expr) => {{
-    $buffer.push($ch);
-  }};
-}
-
-macro_rules! consume_key {
-  ($key:ident, $buffer:ident, $state:ident) => {{
-    $key.clear();
-    $key.push_str(&$buffer);
-    $buffer.clear();
-    $state = State::Value;
-  }};
-}
-
-macro_rules! consume_non_empty_key {
-  ($key:ident, $buffer:ident, $state:ident) => {{
-    $buffer = $buffer.trim().to_string();
-    if !$buffer.is_empty() {
-      consume_key!($key, $buffer, $state);
-    }
-  }};
-}
-
-macro_rules! consume_value {
-  ($output:ident, $key:ident, $buffer:ident, $state:ident) => {{
-    $output.kv.insert($key.clone(), $buffer.clone());
-    $key.clear();
-    $buffer.clear();
-    $state = State::Key;
-  }};
-}
-
-macro_rules! consume_non_empty_value {
-  ($output:ident, $key:ident, $buffer:ident, $state:ident) => {{
-    $buffer = $buffer.trim().to_string();
-    if !$buffer.is_empty() {
-      consume_value!($output, $key, $buffer, $state);
-    }
-  }};
-}
-
-macro_rules! consume_escaped_quotation_mark {
-  ($chars:ident, $buffer:ident, $marker:ident) => {{
-    if let Some(ch) = $chars.peek() {
-      if *ch == $marker {
-        $buffer.push($marker);
-        $chars.next();
-      } else {
-        $buffer.push('\\');
-      }
-    } else {
-      $buffer.push('\\');
-    }
-  }};
-}
-
-macro_rules! clear_buffer {
-  ($buffer:ident, $state:ident, $next_state:expr) => {{
-    $buffer.clear();
-    $state = $next_state;
-  }};
-}
 
 /// Loads key-value pairs from string in KIVI format.
 ///
@@ -111,62 +42,135 @@ pub fn load_from_file<P: AsRef<Path>>(path: P) -> io::Result<KeyValuePairs> {
   Ok(load_from_string(&fs::read_to_string(path)?))
 }
 
-struct Loader {}
+/// Loader states.
+#[derive(Copy, Clone)]
+enum State {
+  Key,
+  KeyExt,
+  Value,
+  ValueExt,
+}
+
+struct Loader {
+  state: State,
+  buffer: String,
+  key: String,
+  marker: char,
+  output: KeyValuePairs,
+}
 
 impl Loader {
   /// Created a loader with default settings.
   fn new() -> Self {
-    Loader {}
+    Loader {
+      state: State::Key,
+      buffer: String::new(),
+      key: String::new(),
+      marker: 0 as char,
+      output: KeyValuePairs::new(),
+    }
   }
 
   /// Loads key-value pairs from string.
-  fn load(&mut self, input: &str, markers: &[char]) -> KeyValuePairs {
-    let mut output = KeyValuePairs::new();
-    let mut state = State::Key;
-    let mut buffer = String::new();
-    let mut key = String::new();
-    let mut chars = input.chars().peekable();
-    let mut marker = 0 as char;
+  fn load(mut self, input: &str, markers: &[char]) -> KeyValuePairs {
+    let chars = &mut input.chars().peekable();
     while let Some(ch) = chars.next() {
-      match state {
+      match self.state {
         State::Key => match ch {
-          ch if self.is_quotation_marker(ch, markers) => {
-            marker = ch;
-            clear_buffer!(buffer, state, State::KeyExt);
+          ch if self.is_allowed_marker(ch, markers) => {
+            self.marker = ch;
+            self.clear_buffer(State::KeyExt);
           }
-          '\n' => consume_non_empty_key!(key, buffer, state),
-          other => consume_char!(buffer, other),
+          '\n' => self.consume_non_empty_key(),
+          other => self.consume_char(other),
         },
         State::KeyExt => match ch {
-          ch if ch == marker => consume_key!(key, buffer, state),
-          '\\' => consume_escaped_quotation_mark!(chars, buffer, marker),
-          other => consume_char!(buffer, other),
+          ch if self.is_marker(ch) => self.consume_key(),
+          '\\' => self.consume_escaped_quotation_mark(chars),
+          other => self.consume_char(other),
         },
         State::Value => match ch {
-          ch if self.is_quotation_marker(ch, markers) => {
-            marker = ch;
-            clear_buffer!(buffer, state, State::ValueExt);
+          ch if self.is_allowed_marker(ch, markers) => {
+            self.marker = ch;
+            self.clear_buffer(State::ValueExt);
           }
-          '\n' => consume_non_empty_value!(output, key, buffer, state),
+          '\n' => self.consume_non_empty_value(),
           other => {
-            consume_char!(buffer, other);
+            self.consume_char(other);
             if chars.peek().is_none() {
-              consume_non_empty_value!(output, key, buffer, state);
+              self.consume_non_empty_value();
             }
           }
         },
         State::ValueExt => match ch {
-          ch if ch == marker => consume_value!(output, key, buffer, state),
-          '\\' => consume_escaped_quotation_mark!(chars, buffer, marker),
-          other => consume_char!(buffer, other),
+          ch if self.is_marker(ch) => self.consume_value(),
+          '\\' => self.consume_escaped_quotation_mark(chars),
+          other => self.consume_char(other),
         },
       }
     }
-    output
+    self.output
   }
 
-  /// Returns `true` when specified character is a quotation marker.
-  fn is_quotation_marker(&self, ch: char, markers: &[char]) -> bool {
+  /// Consumes the specified character.
+  fn consume_char(&mut self, ch: char) {
+    self.buffer.push(ch);
+  }
+
+  fn consume_key(&mut self) {
+    self.key.clear();
+    self.key.push_str(&self.buffer);
+    self.buffer.clear();
+    self.state = State::Value;
+  }
+
+  fn consume_non_empty_key(&mut self) {
+    self.buffer = self.buffer.trim().to_string();
+    if !self.buffer.is_empty() {
+      self.consume_key();
+    }
+  }
+
+  fn consume_escaped_quotation_mark(&mut self, chars: &mut Peekable<Chars>) {
+    if let Some(ch) = chars.peek() {
+      if *ch == self.marker {
+        self.buffer.push(self.marker);
+        chars.next();
+      } else {
+        self.buffer.push('\\');
+      }
+    } else {
+      self.buffer.push('\\');
+    }
+  }
+
+  fn consume_non_empty_value(&mut self) {
+    self.buffer = self.buffer.trim().to_string();
+    if !self.buffer.is_empty() {
+      self.consume_value();
+    }
+  }
+
+  fn consume_value(&mut self) {
+    self.output.kv.insert(self.key.clone(), self.buffer.clone());
+    self.key.clear();
+    self.buffer.clear();
+    self.state = State::Key;
+  }
+
+  /// Clears the input buffer and switches to the next state.
+  fn clear_buffer(&mut self, next_state: State) {
+    self.buffer.clear();
+    self.state = next_state;
+  }
+
+  /// Returns `true` when specified character is an allowed marker.
+  fn is_allowed_marker(&self, ch: char, markers: &[char]) -> bool {
     markers.contains(&ch)
+  }
+
+  /// Returns `true` when specified character is current marker.
+  fn is_marker(&self, ch: char) -> bool {
+    ch == self.marker
   }
 }
